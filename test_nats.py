33d6175584d6f5ce5fc462bbea3479190d9dd5ff
benyissa <benyissa@ostorlab.dev>
2024-09-05 12:24:45 +0100

    use testcontainers

--- test_nats.py
+++ test_nats.py
@@ -1,33 +1,23 @@
-import os
-
 import pytest
 import nats
 import asyncio
+from testcontainers.nats import NatsContainer
 
 
 @pytest.mark.asyncio
 async def test_nats():
-    # Connect to the NATS server
-    nats_url = os.getenv('NATS_SERVER', 'nats://localhost:4222')
-    nc = await nats.connect(nats_url)
-    assert nc.is_connected
+    with NatsContainer() as nats_container:
+        nats_url = nats_container.nats_uri()
 
-    # Callback function to handle messages
-    messages = []
+        nc = await nats.connect(nats_url)
+        assert nc.is_connected
+        messages = []
 
-    async def message_handler(msg):
-        messages.append(msg.data.decode())
+        async def message_handler(msg):
+            messages.append(msg.data.decode())
 
-    # Subscribe to a subject
-    await nc.subscribe("test_subject", cb=message_handler)
-
-    # Publish a message
-    await nc.publish("test_subject", b"Hello, NATS!")
-
-    # Give some time for the message to be processed
-    await asyncio.sleep(1)
-
-    # Check if the message was received
-    assert "Hello, NATS!" in messages
-
-    await nc.close()
+        await nc.subscribe("test_subject", cb=message_handler)
+        await nc.publish("test_subject", b"Hello, NATS!")
+        await asyncio.sleep(1)
+        assert "Hello, NATS!" in messages
+        await nc.close()
